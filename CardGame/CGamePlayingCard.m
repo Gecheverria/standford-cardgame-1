fdf2c7eb936421a6fb501fb9d3df3daef3c940fc
Gabriel Enrique Echeverria Mira <Gabriel@Gabriels-MacBook-Pro.local>
2016-09-13 15:36:20 -0600

    Added missing files

--- CardGame/CGamePlayingCard.m
+++ CardGame/CGamePlayingCard.m
@@ -10,4 +10,45 @@
 
 @implementation CGamePlayingCard
 
+//Sobreescribimos la propiedad contents de CGameCard
+-(NSString *) contents {
+    
+    NSArray *rankStrings = [CGamePlayingCard rankStrings];
+    //los self.ranky self.suit vienen de sus getters, que son llenados en el playingcarddeck.
+    return [rankStrings[self.rank] stringByAppendingString:self.suit];
+    
+}
+
+//A continuacion 3 metodos de clase, que son principalmente de utilidad para definir valores
++ (NSArray *)validSuits {
+    return @[@"♠︎",@"♣︎",@"♥︎",@"♦︎"];
+}
+
++ (NSArray *)rankStrings {
+    return @[@"?",@"1",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9",@"10",@"J",@"Q",@"K"];
+}
+
++ (NSUInteger)maxRanks {
+    return [[self rankStrings] count] - 1;
+}
+
+//Debemos sintetizar suit porque sobreescribimos su getter y setter
+@synthesize suit = _suit;
+
+- (void)setSuit:(NSString *)suit {
+    if ([[CGamePlayingCard validSuits] containsObject:suit]) {
+        _suit = suit;
+    }
+}
+
+- (NSString *) setSuit {
+    return _suit ? _suit :@"?";
+}
+
+- (void)setRank:(NSUInteger)rank {
+    if (rank <= [CGamePlayingCard maxRanks]) {
+        _rank = rank;
+    }
+}
+
 @end
--- CardGame/ViewController.m
+++ CardGame/ViewController.m
@@ -7,21 +7,56 @@
 //
 
 #import "ViewController.h"
+#import "CGamePlayingCardDeck.h"
+#import "CGameCard.h"
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
+@property (nonatomic) int flipCount;
+
+//Necesitamos un deck para poder jugar, por lo que lo creamos como un property
+@property (strong, nonatomic) CGameDeck *deck;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+//No podemos tenes un deck nulo, por lo que hay que iniciarlo mediante lazy instantiation. Iniciamos un PlayingCardDeck porque es ahi donde se genera las cartas a usar del deck.
+- (CGameDeck *)deck {
+    if (!_deck) _deck = [self createDeck];
+    return _deck;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (CGameDeck *)createDeck {
+    return [[CGamePlayingCardDeck alloc] init];
+}
+
+- (void)setFlipCount:(int)flipCount {
+    _flipCount = flipCount;
+    self.flipsLabel.text= [NSString stringWithFormat:@"Flips: %d", self.flipCount];
+}
+
+
+- (IBAction)touchCardButton:(UIButton *)sender {
+    
+    if ([sender.currentTitle length]) {
+        
+        [sender setBackgroundImage:[UIImage imageNamed:@"cardback"] forState:UIControlStateNormal];
+        [sender setTitle:@"" forState:UIControlStateNormal];
+        self.flipCount++;
+        
+    } else {
+        //Una ves creado nuestro deck, creamos una nueva carta, que la extraemos del metodo drawRandomCard
+        CGameCard *randomCard = [self.deck drawRandomCard];
+        
+        if (randomCard) {
+            [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"] forState:UIControlStateNormal];
+            [sender setTitle:randomCard.contents forState:UIControlStateNormal];
+            self.flipCount++;
+        }
+    }
+    
 }
 
 @end
